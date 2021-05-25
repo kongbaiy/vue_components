52b70e4d96970510bb43c5c6e41ee03267c525ca
kebi <kanglu@wondersgroup.com>
2021-05-25 18:19:34 +0800

    新增虚拟列表组件

--- src/app/components/list/VirtualScroller.vue
+++ src/app/components/list/VirtualScroller.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
-    <div :style="{ height }" class="scroll" @scroll="onScroll">
-      <slot></slot>
+  <div class="virtual-scroll-container" :style="{ height }" @scroll="_onScroll">
+    <div class="content" :style="{ height: `${(value.length - 1) * 111}px` }">
+      <div class="viewing-area">
+        <div v-for="(item, index) in list" :key="index" class="slot-box">
+          <slot v-bind="item"></slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import VirtualScrolled from '@/utils/VirtualScroller';
-
 export default {
+  model: {
+    prop: 'value',
+    event: 'update',
+  },
+
   props: {
-    data: {
+    value: {
       type: Array,
       default: () => []
     },
@@ -19,46 +26,70 @@ export default {
       type: String,
       default: '100vh'
     },
-    triggerDistance: {
-      type: Number,
-      default: 100
+    page: {
+      type: [Number, String],
+      default: 1
     }
   },
 
   data() {
     return {
       scrollTopVal: 0,
-      lock: false
+      timeout: '',
+      perNum: 0
+    };
+  },
+
+  mounted() {
+  },
+
+  computed: {
+    list() {
+      const { value, scrollTopVal } = this;
+      const start = Math.floor(scrollTopVal / 110),
+        end = start + 10;
+
+      return value.slice(start, end);
     }
   },
 
   methods: {
-    onScroll({ target }) {
-      const { scrollTop, scrollHeight, offsetHeight } = target;
-      const { data, triggerDistance, scrollTopVal } = this;
-      const handel = VirtualScrolled(data);
+    _onScroll({ target }) {
+      const { scrollTop } = target;
+      const VA = document.querySelector(".viewing-area");
+      const curNum =  Math.floor(scrollTop / 110);
 
-      if (scrollTopVal && scrollTop < scrollTopVal) {
-        if (triggerDistance && scrollTop <= triggerDistance) {
-        }
+      if (this.scrollTopVal && scrollTop < this.scrollTopVal) {
+
       } else {
-        if (scrollTop >= scrollHeight - offsetHeight - triggerDistance && !this.lock) {
-          this.lock = true;
-          handel((data) => {
-            this.lock = false;
-            this.$emit('add', data);
-          });
-        }
+
+      }
+
+
+      // VA.setAttribute('style', `margin-top:(${scrollTop}px)`);
+      if(this.perNum != curNum || scrollTop < 56) {
+        this.perNum = curNum;
+        VA.setAttribute('style', `transform: translateY(${scrollTop}px)`);
       }
       this.scrollTopVal = scrollTop;
+
     },
-  }
+
+    _antiShake(callback) {
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        callback();
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  .scroll {
-    overflow-y: scroll;
-    -webkit-overflow-scrolling: touch;
+.virtual-scroll-container {
+  overflow: auto;
+  .content {
+    overflow: hidden;
   }
+}
 </style>
--- src/app/pages/demo/List.vue
+++ src/app/pages/demo/List.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <!-- <ul class="list" @scroll="onListScroll">
-      <li class="list-item" v-for="(item, index) in list" :key="index">
-        {{ item.title }}
-      </li>
-    </ul> -->
-    <virtual-scroller :data="list" @add="onAdd">
-      <div class="list-item" v-for="(item, index) in list" :key="index">{{ item.title }}</div>
+    <h1>virtual list</h1>
+    <virtual-scroller v-model="list" :page="page" @load="onLoad">
+      <template slot-scope="scope">
+        <div class="list-item">
+          {{scope.title}}{{scope.index}}
+      </div>
+      </template>
     </virtual-scroller>
   </div>
 </template>
@@ -14,111 +14,52 @@
 <script>
 export default {
   components: {
-    VirtualScroller: () => import('@/app/components/list/VirtualScroller')
+    VirtualScroller: () => import('@/app/components/list/VirtualScroller'),
   },
 
   data() {
     return {
+      page: 1,
       list: [
-        { title: "list title1" },
-        { title: "list title2" },
-        { title: "list title3" },
-        { title: "list title4" },
-        { title: "list title5" },
-        { title: "list title6" },
-        { title: "list title7" },
-        { title: "list title8" },
-        { title: "list title9" },
-        { title: "list title10" },
-        { title: "list title11" },
-        { title: "list title12" },
-        { title: "list title13" },
-        { title: "list title14" },
-        { title: "list title15" },
-        { title: "list title16" },
-        { title: "list title17" },
-        { title: "list title18" },
-        { title: "list title19" },
-        { title: "list title20" },
-      ],
-      scrollTop: 0,
-      startSetTimeout: "",
-      lock: false
+      ]
     };
   },
 
-  mounted() {},
+  mounted() {
+    let arr = [];
+    for(let i = 0;i < 20; i++) {
+      arr.push({
+        title: 'list',
+        index: i + 1
+      });
+    }
+     this.list = arr;
+  },
 
   methods: {
-    // onListScroll({ target }) {
-    //   const { scrollTop, scrollHeight, offsetHeight } = target;
-    //   const scroll_val = 110;
-
-    //   if (this.scrollTop && scrollTop < this.scrollTop) {
-    //     console.log("top");
-
-    //     if (scroll_val && scrollTop <= scroll_val) {
-    //       console.log("top trigger");
-    //     }
-    //   } else {
-    //     console.log("bottom");
-
-    //     if (scrollTop >= scrollHeight - offsetHeight - scroll_val && !this.lock) {
-    //       let length = this.list.length;
-
-    //       this.lock = true;
-    //       console.log("bottom trigger");
-    //       clearTimeout(this.startSetTimeout);
-    //       this.list = this.list.slice(length / 2);
-    //       this.startSetTimeout = setTimeout(callback => {
-    //         let arr = [];
-
-    //         for(let i = 0; i < 15; i++) {
-    //           arr.push({ title: 'list title' + (length + i + 1) });
-    //         }
-    //         this.list = this.list.concat(arr);
-    //         this.lock = false;
-    //       }, 600);
-    //     }
-    //   }
-    //   this.scrollTop = scrollTop;
-    // },
-
-    onAdd(data) {
-      let arr = [], length = this.list.length;
-
-      for(let i = 0; i < 15; i++) {
-        arr.push({ title: 'list title' + (length + i + 1) });
-      }
-      this.$nextTick(callback => {
-      this.list = data;
-
-         this.list = this.list.concat(arr);
-      });
+    onLoad({ direction }) {
+      // if (direction === 'up') {
+      //   if(this.page < this.list.length - 1) {
+      //     this.page ++;
+      //   }
+      // } else {
+      //   if(this.page > 1) {
+      //     this.page --;
+      //   }
+      // }
+      // console.log(this.page);
     }
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.list {
-  margin: 20px;
-  overflow-y: scroll;
-  -webkit-overflow-scrolling: touch;
-  height: calc(100vh - 40px);
-  .list-item {
-    height: 60px;
-    border-bottom: 1px solid;
-    border-color: rgba(0, 0, 0, 0.1);
-    font-size: 20px;
-    line-height: 60px;
-  }
-}
 .list-item {
-    height: 60px;
-    border-bottom: 1px solid;
-    border-color: rgba(0, 0, 0, 0.1);
-    font-size: 20px;
-    line-height: 60px;
-  }
+  height: 110px;
+  background-color: #ddd;
+  border-bottom: 1px solid;
+  border-color: rgba(0, 0, 0, 0.1);
+  font-size: 30px;
+  line-height: 110px;
+}
 </style>
